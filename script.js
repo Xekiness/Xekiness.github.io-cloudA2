af0af55e5cca379da6b2a7643bf6d2861baa0e1b
Xekiness <93179789+Xekiness@users.noreply.github.com>
2023-08-07 06:48:47 +0800

    Update script.js
    Assignment 2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 alert("Github is linked.");
+
+/*JS for hamMenu*/
+const hamBtn = document.querySelector("#hamIcon");
+hamBtn.addEventListener("click", toggleMenus);
+const menuItemsList=document.querySelector("nav ul");
+
+function toggleMenus(){ /*open and close menu*/
+    if (menuLinks.style.display === "block") 
+        menuLinks.style.display = "none";
+    else menuLinks.style.display = "block";
+}
